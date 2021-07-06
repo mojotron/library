aed0b1e07630b5d28a76163bfff465d0c6208fde
mojotron <igormalj@gmail.com>
2021-07-06 23:09:05 +0200

    Add book object constructor, add minor style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+//Crud application
+//Crate book, read book(display), update book, delete book.
+//Search for book with api
+//Get book data from api and with that data creat book object
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+Book.prototype.renderBookHtml = function () {
+  const bookDiv = document.createElement("div");
+  bookDiv.innerHTML = `
+    <div class="book-object">
+      <h2>${this.title}</h2>
+      <p>${this.author}</p>
+    </div>
+  `;
+  return bookDiv;
+};
+
+const hobbit = new Book("The Hobbit", "J. R. R. Tolkien");
+const mainWrapper = document.querySelector(".main-wrapper");
+mainWrapper.insertAdjacentElement("beforeend", hobbit.renderBookHtml());
