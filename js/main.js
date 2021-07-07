9be506acbaec36fbe848bcd966e76f640a50fb26
mojotron <igormalj@gmail.com>
2021-07-07 22:18:43 +0200

    Add basic book object structure and style

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //Crate book, read book(display), update book, delete book.
 //Search for book with api
 //Get book data from api and with that data creat book object
-
+const library = [];
 function Book(title, author) {
   this.title = title;
   this.author = author;
@@ -20,6 +20,11 @@ Book.prototype.renderBookHtml = function () {
   return bookDiv;
 };
 
-const hobbit = new Book("The Hobbit", "J. R. R. Tolkien");
-const mainWrapper = document.querySelector(".main-wrapper");
-mainWrapper.insertAdjacentElement("beforeend", hobbit.renderBookHtml());
+// const hobbit = new Book("The Hobbit", "J. R. R. Tolkien");
+// const mainWrapper = document.querySelector(".main-wrapper");
+// mainWrapper.insertAdjacentElement("beforeend", hobbit.renderBookHtml());
+const request = new XMLHttpRequest();
+request.open(
+  "GET",
+  `https://openlibrary.org/books/OL7353617M/Fantastic_Mr._Fox`
+);
