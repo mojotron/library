d1284f9d6d6e1718dca2c5818d6a17ab4e66b1a8
mojotron <igormalj@gmail.com>
2021-07-10 14:17:46 +0200

    Add submit form handler and book display logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,59 @@
 "use strict";
+const library = [];
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.renderBookHtml = function () {
+  const bookHTML = document.createElement("div");
+  bookHTML.innerHTML = `
+  <div class="book-object">
+    <div class="book-cover">
+      <i class="fas fa-book-open"></i>
+    </div>
+    <div class="book-info">
+      <h3>${this.title}</h3>
+      <span class="book-author">by ${this.author}</span>
+      <p class="book-pages">Number of pages, ${this.pages}.</p>
+      <div class="book-options">
+        <p>Read</p>
+        <span class="book-btn btn-read-checkbox">${
+          this.read
+            ? `<i class="fas fa-check"></i>`
+            : `<i class="fas fa-times"></i>`
+        }</span>
+        <span class="book-btn btn-update-book"><i class="fas fa-pen"></i></span>
+        <span class="book-btn btn-delete-book"><i class="fas fa-trash"></i></span>
+      </div>
+  </div>
+  `;
+  return bookHTML;
+};
 //DOM Selectors
+const booksShowcase = document.querySelector(".books-showcase");
 const addBookBtn = document.querySelector(".btn-add-book");
 const inputForm = document.querySelector(".add-book-form");
+const inputFields = document.querySelectorAll(".book-data");
+
+inputForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const bookInfo = Array.from(inputFields).map((ele) => {
+    return ele.type === "checkbox" ? ele.checked : ele.value;
+  });
+  const newBook = new Book(...bookInfo);
+  library.push(newBook);
+  booksShowcase.appendChild(newBook.renderBookHtml());
+
+  library.forEach((book) => book.renderBookHtml());
+  //Reset to original state
+  inputFields.forEach(function (ele) {
+    ele.type === "checkbox" ? (ele.checked = false) : (ele.value = "");
+  });
+  inputForm.classList.add("hidden");
+});
 
 const callDisplayForm = () => inputForm.classList.remove("hidden");
 addBookBtn.addEventListener("click", callDisplayForm);
@@ -13,30 +65,7 @@ function addNewBook() {
   //Add book to library
   //Update library display
 }
-const library = [];
-function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.renderBookHtml = function () {
-  const bookDiv = document.createElement("div");
-  bookDiv.innerHTML = `
-    <div class="book-object">
-      <h2>${this.title}</h2>
-      <p>${this.author}</p>
-    </div>
-  `;
-  return bookDiv;
-};
 
 // const hobbit = new Book("The Hobbit", "J. R. R. Tolkien");
 // const mainWrapper = document.querySelector(".main-wrapper");
 // mainWrapper.insertAdjacentElement("beforeend", hobbit.renderBookHtml());
-const request = new XMLHttpRequest();
-request.open(
-  "GET",
-  `https://openlibrary.org/books/OL7353617M/Fantastic_Mr._Fox`
-);
