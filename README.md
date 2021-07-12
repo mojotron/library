6ea7c96a7a781a8aabac21b74b4212657428a768
mojotron <igormalj@gmail.com>
2021-07-12 21:39:26 +0200

    Refactor adding books to page - solve issue for adding event handlers for read button

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Goal of this project is to learn object orientated programming.
 - Object Protptype
 - Prototypal inferitence
 - KISS principle - I wanted to much from the start and soon I was to deep and didn't saw head nor tail of the project. After realization of this I remeberd KISS principle (Keep It Stupidly Simple) and start to implement simple logic and after that I can start add more complicated features.
+  parentNode.changeElement(new; old)
--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 "use strict";
 function Library() {
-  this.length = 0;
+  this.bookId = 0;
   this.bookcase = {};
 }
-Library.prototype.incrementLength = function () {
-  this.length++;
+Library.prototype.incrementBooId = function () {
+  this.bookId++;
 };
 
-Library.prototype.getLength = function () {
-  return this.length;
+Library.prototype.getBookId = function () {
+  return this.bookId;
 };
 
 Library.prototype.addBook = function (book) {
-  //Create and display Dom element
-  const bookDOMElement = book.renderBookHtml(this.length);
-  booksShowcase.appendChild(bookDOMElement);
-  //Save book to library
-  this.bookcase[this.getLength()] = book;
-  this.incrementLength();
-  //Attach event handlers to DOM book object
-  const read = bookDOMElement.querySelector(".btn-read-book");
-  const update = bookDOMElement.querySelector(".btn-update-book");
-  const del = bookDOMElement.querySelector(".btn-delete-book");
-  read.addEventListener("click", () => alert("R"));
-  update.addEventListener("click", () => alert("U"));
-  del.addEventListener("click", () => alert("D"));
+  this.bookcase[this.getBookId()] = book;
+  this.incrementBooId();
+  this.listBooks();
 };
+
 Library.prototype.deleteBook = function (id) {
   delete this.bookcase[id];
+};
+Library.prototype.listBooks = function () {
+  const booksShowcase = document.querySelector(".books-showcase");
+  booksShowcase.innerHTML = "";
+  for (let book in this.bookcase) {
+    let bookEle = this.bookcase[book].renderBookHtml(book);
+    booksShowcase.appendChild(bookEle);
+    const read = bookEle.querySelector(".btn-read-book");
+    const update = bookEle.querySelector(".btn-update-book");
+    const del = bookEle.querySelector(".btn-delete-book");
+    read.addEventListener("click", (e) => {
+      library.bookcase[getBookObjectId(e)].toggleRead();
+      this.listBooks();
+    });
+    update.addEventListener("click", () => alert("U"));
+    del.addEventListener("click", (e) => {
+      delete library.bookcase[getBookObjectId(e)];
+      this.listBooks();
+    });
+  }
 };
 function Book(title, author, pages, read) {
   this.title = title;
@@ -37,7 +48,8 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.toggleRead = function () {
-  this.read = !this.read;
+  const temp = this.read;
+  this.read = !temp;
 };
 
 Book.prototype.renderBookHtml = function (id) {
@@ -50,17 +62,11 @@ Book.prototype.renderBookHtml = function (id) {
       <span class="book-author">by ${this.author}</span>
       <p class="book-pages">Number of pages, ${this.pages}.</p>
       <div class="book-options">
-        <div class="book-btn btn-read-book">
-          <p>Read</p>
-          <span>${
-            this.read
-              ? `<i class="fas fa-check"></i>`
-              : `<i class="fas fa-times"></i>`
-          }</span>
-        </div>
-        
-        <span class="book-btn btn-update-book"><i class="fas fa-pen"></i></span>
-        <span class="book-btn btn-delete-book"><i class="fas fa-trash"></i></span>
+      <i class="book-btn btn-read-book ${
+        this.read ? `fas fa-check` : `fas fa-times`
+      }"></i>
+        <i class="book-btn btn-update-book fas fa-pen"></i>
+        <i class="book-btn btn-delete-book fas fa-trash"></i>
       </div>
   </div>
   `;
@@ -69,11 +75,15 @@ Book.prototype.renderBookHtml = function (id) {
 //Library Object for user books
 const library = new Library();
 //DOM Selectors
-const booksShowcase = document.querySelector(".books-showcase");
+
 const addBookBtn = document.querySelector(".btn-add-book");
 const cancelForm = document.querySelector(".btn-cancel-form");
 const inputForm = document.querySelector(".add-book-form");
 const inputFields = document.querySelectorAll(".book-data");
+
+function getBookObjectId(event) {
+  return event.target.parentElement.parentElement.parentElement.dataset.bookId;
+}
 
 const toggleDisplayForm = () => inputForm.classList.toggle("hidden");
 
@@ -97,7 +107,7 @@ function resetAndHideForm(inputFields) {
 function createAndDisplayBook(event) {
   event.preventDefault();
   const newBook = new Book(...getBookInputData(inputFields));
-  const bookId = library.getLength();
+  const bookId = library.getBookId();
   library.addBook(newBook);
   resetAndHideForm(inputFields);
 }
@@ -108,14 +118,8 @@ cancelForm.addEventListener("click", () => resetAndHideForm(inputFields));
 
 //////////////////////////////////////////////
 //Template data
-const endersGame = new Book("Ender's Game", "Orson Scott Card", 324, true);
-const gunslinger = new Book(
-  "The Dark Tower: The Gunslinger",
-  "Stephen King",
-  300,
-  true
+library.addBook(new Book("Ender's Game", "Orson Scott Card", 324, true));
+library.addBook(
+  new Book("The Dark Tower: The Gunslinger", "Stephen King", 300, true)
 );
-const hobbit = new Book("The Hobbit", "J. R. R. Tolkien", 310, true);
-library.addBook(endersGame);
-library.addBook(gunslinger);
-library.addBook(hobbit);
+library.addBook(new Book("The Hobbit", "J. R. R. Tolkien", 310, true));
